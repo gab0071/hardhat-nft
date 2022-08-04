0951a9fef9eaccad519695450b7593926cef0b29
catella <jeicarm7@gmail.com>
2022-08-04 11:12:08 -0500

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,74 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+
+module.exports = {
+    //solidity: "0.8.7",
+    solidity: {
+        compilers: [
+            { version: "0.8.8" },
+            { version: "0.6.6" },
+            { version: "0.4.19" },
+            { version: "0.6.12" },
+            { version: "0.6.0" },
+        ],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+        },
+        rinkeby: {
+            chainId: 4,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+
+        kovan: {
+            chainId: 42,
+            url: KOVAN_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            //rinkeby: ETHERSCAN_API_KEY,
+            kovan: ETHERSCAN_API_KEY,
+            //polygon: POLYGONSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 500000, // 500 seg max
+    },
+}
